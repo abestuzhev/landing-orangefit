01ec3d3202e20148ed2bb15f0b4a5bc991eea3de
Aleksey Bestuzhev <aleksey.bestuzhev@gmail.com>
2016-12-21 13:54:05 +0300

    changes

--- js/scroll.js
+++ js/scroll.js
@@ -9,23 +9,23 @@ jQuery(document).ready(function () {
         if(jQuery(window).scrollTop()>jQuery('.header p.button').offset().top) {
             jQuery('.header_top .button_').css('opacity','1');
             jQuery('.header_top .button_ input').css('pointer-events','all');
-            jQuery('.timer').appendTo('.timer-block');
+            // jQuery('.timer').appendTo('.timer-block');
         }
         else {
             jQuery('.header_top .button_').css('opacity','0');
             jQuery('.header_top .button_ input').css('pointer-events','none');
-            jQuery('.header_top .timer').prependTo('.timer-big-block');
+            // jQuery('.header_top .timer').prependTo('.timer-big-block');
 
         }
 
         if(jQuery(window).width()<768) {
             if(jQuery(window).scrollTop()>jQuery('.header p.button').offset().top) {
                 jQuery('.header_top .button_').css('display','block');
-                jQuery('.timer').appendTo('.timer-block');
+                // jQuery('.timer').appendTo('.timer-block');
             }
             else {
                 jQuery('.header_top .button_').css('display','none');
-                jQuery('.header_top .timer').prependTo('.timer-big-block');
+                // jQuery('.header_top .timer').prependTo('.timer-big-block');
             }
         }
     }
